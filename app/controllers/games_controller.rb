b0640f5b6a8c792648cc9e28c4b281100738d507
Rodolphe5537 <rodolphe@dedorlodot.net>
2019-02-25 17:58:27 +0100

    ok

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,11 @@
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times {@letters << ('a'..'z').to_a.sample}
+  end
+
+  def score
+    @letters = params[:letters]
+    @word = params[:word]
+  end
+end
